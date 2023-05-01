39a8fccf63a65234492d93277f5651946f0177eb
mfanjaud <marion.bikoi@gmail.com>
2023-05-01 21:11:09 +0200

    http request

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -6,19 +6,41 @@ async function httpGetPlanets() {
   return await res.json();
 }
 
+// Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
-  // TODO: Once API is ready.
-  // Load launches, sort by flight number, and return as JSON.
+  const res = await fetch(`${API_URL}/launches`);
+  const fetchedLaunches = await res.json();
+  return fetchedLaunches.sort((a, b) => a.flightNumber - b.flightNumber);
 }
 
+// Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
-  // TODO: Once API is ready.
-  // Submit given launch data to launch system.
+  try {
+    const submitLaunch = await fetch(`${API_URL}/launches`, {
+      method: "post",
+      body: JSON.stringify(launch),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    return submitLaunch;
+  } catch (err) {
+    return {
+      ok: false,
+    };
+  }
 }
 
+// Delete launch with given ID.
 async function httpAbortLaunch(id) {
-  // TODO: Once API is ready.
-  // Delete launch with given ID.
+  try {
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: "delete",
+    });
+  } catch (err) {
+    return {
+      ok: false,
+    };
+  }
 }
 
 export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
